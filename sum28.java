d41fb282a90e2bf5743a4a83fdc92ff70f137227
Ella Lee <elee80@stuy.edu>
2026-09-16 21:52:50 -0400

    fixed up the code

--- sum28.java
+++ sum28.java
@@ -5,23 +5,24 @@
 //sum28([1, 2, 3, 4]) → false
 //sum28([2, 3, 2, 2, 4, 2, 2]) → false
 
-public class sum28Function{
-  public boolean sum28(int[] nums){
+public class sum28{
+  public static boolean sum28(int[] nums){
   int sum = 0;
   for (int i = 0; i < nums.length; i++){
     if (nums[i] == 2){
       sum = sum + 2;
     }
   }
-  }
+
   if (sum == 8){
     return true;
   }
   return false;
 }
 
-public static void main(string[] args){
-  System.out.println(sum28([2, 3, 2, 2, 4, 2]));
-  System.out.println(sum28([1, 2, 3, 4]));
-  System.out.println(sum28([2, 3, 2, 2, 4, 2, 2]));
+public static void main(String[] args){
+  System.out.println(sum28(new int[] {2, 3, 2, 2, 4, 2}));
+  System.out.println(sum28(new int[] {1, 2, 3, 4}));
+  System.out.println(sum28(new int[] {2, 3, 2, 2, 4, 2, 2}));
+}
 }
