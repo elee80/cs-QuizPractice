27e945d073d0f7390a5e76169671d48da871b9d3
Ella Lee <elee80@stuy.edu>
2026-09-16 22:04:45 -0400

    added code

---
+++ scoresIncreasing.java
@@ -0,0 +1,24 @@
+//Given an array of scores, return true if each score is equal or greater than the one before. The array will be length 2 or more.
+
+
+//scoresIncreasing([1, 3, 4]) → true
+//scoresIncreasing([1, 3, 2]) → false
+//scoresIncreasing([1, 1, 4]) → true
+
+public class scoresIncreasing{
+  public static boolean scoresIncreasing(int[] scores){
+    for (int i = 0; i < scores.length; i++){
+      if (scores[i] <= scores[i + 1]){
+        return true;
+      }
+    }
+    return false;
+  }
+
+
+public static void main(String[] args){
+  System.out.println(scoresIncreasing(new int[] {1, 3, 4}));
+  System.out.println(scoresIncreasing(new int[] {1, 3, 2}));
+  System.out.println(scoresIncreasing(new int[] {1, 1, 4}));
+}
+}
